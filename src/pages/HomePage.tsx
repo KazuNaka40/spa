5af51e3685b95a21ddb425e9d3cad795f21a0441
sukeko <venusgeneration@gmail.com>
2020-10-12 07:40:58 +0900

    run shiteruyo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import React, { useState } from "react";
-const React = require("react");
 
 function HomeButton() {
   let history = useHistory();
